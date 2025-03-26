b17a281f5f5ba67eed3634b690c24ad090d84949
Dylan Hunter <dylanhunter34@gmail.com>
2025-03-26 19:00:52 -0400

    Enhance coin system: add daily claim logic, multiplier checks, and balance command

--- cogs/Coins.py
+++ cogs/Coins.py
@@ -20,16 +20,19 @@ def save_user_data(data):
 
 def should_claim_daily(user_data):
     today = str(datetime.datetime.now().date())
+    if user_data['last_claimed'] != today:
+        return True
+    return False
+
+def increase_multiplier(user_data):
+    today = str(datetime.datetime.now().date())
     yesterday = str(datetime.datetime.now().date() - datetime.timedelta(days=1))
     if user_data['last_claimed'] == yesterday:
         user_data['multiplier'] += 0.1
     else:
         user_data['multiplier'] = 1.0
-    if user_data['last_claimed'] != today:
-        user_data['last_claimed'] = today
-        user_data['coins'] += 100 * user_data['multiplier']
-        return True
-    return False
+    user_data['multiplier'] = round(user_data['multiplier'], 1)
+    user_data['last_claimed'] = today
 
 class Coins(commands.Cog):
     def __init__(self, bot):
@@ -62,7 +65,7 @@ class Coins(commands.Cog):
             await ctx.send(f'{ctx.author.mention} You are already opted in! Remember to use .coins to check your balance and .daily to earn coins daily!')
 
     #cheack coin balance
-    @commands.command(aliases=['bal', 'money', 'coins'])
+    @commands.command(aliases=['bal', 'money', 'coins', 'balance'])
     async def check_balance(self, ctx):
         print(f"Balance check called by {ctx.author.name}")
         user_id = str(ctx.author.id)
@@ -80,10 +83,24 @@ class Coins(commands.Cog):
             await ctx.send(f'{ctx.author.mention} You are not opted in! Use .opt to start earning coins!')
         else:
             if should_claim_daily(self.user_data[user_id]):
+                self.user_data[user_id]['coins'] += (100 * self.user_data[user_id]['multiplier'])
+                self.user_data[user_id]['coins'] = round(self.user_data[user_id]['coins'], 1)
+                increase_multiplier(self.user_data[user_id])
                 save_user_data(self.user_data)
                 await ctx.send(f'{ctx.author.mention} You have claimed your daily coins! Use .coins to check your balance!')
             else:
                 await ctx.send(f'{ctx.author.mention} You have already claimed your daily coins! Please wait until tomorrow to claim again!')
+    
+    #check current multiplier
+    @commands.command()
+    async def multiplier(self, ctx):
+        print(f"Multiplier check called by {ctx.author.name}")
+        user_id = str(ctx.author.id)
+        if user_id not in self.user_data:
+            await ctx.send(f'{ctx.author.mention} You are not opted in! Use .opt to start earning coins!')
+        else:
+            await ctx.send(f'{ctx.author.mention} Your current multiplier is {self.user_data[user_id]["multiplier"]}x!')
+
     
 async def setup(bot):
     await bot.add_cog(Coins(bot))
