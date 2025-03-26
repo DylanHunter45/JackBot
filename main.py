501896ca840a30745b8fc2f865ec37f81f327f94
Dylan Hunter <dylanhunter34@gmail.com>
2025-03-25 21:01:50 -0400

    Add hello command and on_ready event to Discord bot

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-
+token = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-bot.run(os.getenv("TOKEN"))
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello, {ctx.author.mention}!")   
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+bot.run(token=token)
