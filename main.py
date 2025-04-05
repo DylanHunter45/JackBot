987671cda76a3ccc57693c8cabd226f51a3808fa
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-04 20:01:19 -0400

    Refactor loading logic: enable conditional loading of beta cogs with logging for clarity

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 logger = JackBotLogger(log_file_path="jackbot.log", log_level=logging.INFO)
 logger.log_info("Bot is starting...")
 
+BETA_COGS = ['BlackJack.py']
+INCLUDE_BETA_COGS = False
+
 async def load():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
-            # if filename == 'BlackJack.py':
-            #     continue
+            if filename in BETA_COGS and not INCLUDE_BETA_COGS:
+                logger.log_info(f'{filename} is a beta cog and will not be loaded.')
+                continue
             await bot.load_extension(f'cogs.{filename[:-3]}')
             logger.log_info(f'{filename} has been loaded!')
 
